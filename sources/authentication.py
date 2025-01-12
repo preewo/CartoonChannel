772168e0c91fea279c0ecaae16ba0597b49bfe9e
preewo <preewosec@gmail.com>
2025-01-12 10:43:44 -0500

    #added authentication script seperatly

---
+++ sources/authentication.py
@@ -0,0 +1,16 @@
+import requests
+from sources.config import LOGIN_URL,AUTH_DATA
+
+def authenticate():
+    response = requests.post(LOGIN_URL, json=AUTH_DATA, verify=False)
+
+    if response.status_code == 200:
+        access_token = response.json()['data']['token']
+        print(f"Authenticated! Token: {access_token}")
+        headers = {
+            'Authorization': f'Bearer {access_token}'
+        }
+    else:
+        print("xxxxxxx - Authentication Failed!!! - xxxxxxx")
+        return None
+    return headers
